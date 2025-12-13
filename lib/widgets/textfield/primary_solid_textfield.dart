f0043ac9285e5ead12e9fcd2ba4e91d4ee57d165
Ganjarganda <ganjargandag@gmail.com>
2025-12-13 13:10:36 +0700

    gg

---
+++ lib/widgets/textfield/primary_solid_textfield.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class PrimarySolidTextfield extends StatelessWidget {
+  final String? hintText;
+  final TextInputAction? action;
+  final TextInputType? type;
+  final bool secureText;
+  final TextEditingController? controller;
+  final bool readOnly;
+  final Function? onClick;
+  final bool disableMaxLine;
+  final TextCapitalization? capitalization;
+  final bool showSuffixIcon;
+  final Function? onEditComplete;
+  final Function(String)? onChange;
+  final Function(String)? onSubmit;
+  final Color? textColor;
+  final Color? hintTextColor;
+  final Widget? suffixIcon;
+  final Widget? prefixIcon;
+  final FocusNode? focusNode;
+  final bool? autoFocus;
+  final int? maxLength;
+  final Color? backgroundColor;
+  final EdgeInsets? contentPadding;
+  final List<TextInputFormatter>? inputFormatter;
+  final bool enable;
+  final Iterable<String>? autoFill;
+
+  const PrimarySolidTextfield({
+    super.key,
+    required this.hintText,
+    required this.action,
+    required this.type,
+    this.secureText = false,
+    required this.controller,
+    this.readOnly = false,
+    this.onClick,
+    this.disableMaxLine = false,
+    this.capitalization,
+    this.showSuffixIcon = false,
+    this.onEditComplete,
+    this.onChange,
+    this.onSubmit,
+    this.textColor,
+    this.hintTextColor,
+    this.suffixIcon,
+    this.prefixIcon,
+    this.focusNode,
+    this.autoFocus = false,
+    this.maxLength,
+    this.backgroundColor,
+    this.contentPadding,
+    this.inputFormatter,
+    this.enable = true,
+    this.autoFill,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: TextField(
+        enabled: enable,
+        readOnly: readOnly,
+        onTap: () => onClick != null ? onClick!() : {},
+        controller: controller,
+        textInputAction: action,
+        keyboardType: type,
+        autofillHints: autoFill,
+        obscureText: secureText,
+        focusNode: focusNode,
+        inputFormatters: inputFormatter,
+        autofocus: autoFocus!,
+        maxLines: disableMaxLine ? 5 : 1,
+        maxLength: maxLength,
+        onChanged: (value) => onChange != null ? onChange!(value) : {},
+        onSubmitted: (value) => onSubmit != null ? onSubmit!(value) : {},
+        onEditingComplete:
+            () => onEditComplete != null ? onEditComplete!() : {},
+        style: styleSubtitle.copyWith(
+          color: textColor ?? blackColor,
+          fontSize: screenWidth < screenHeight ? 16.sp : 10.sp,
+        ),
+        decoration: InputDecoration(
+          hintText: hintText,
+          contentPadding:
+              contentPadding ??
+              EdgeInsets.symmetric(
+                vertical: screenWidth < screenHeight ? 4.w : 2.h,
+                horizontal: screenWidth < screenHeight ? 2.w : 1.h,
+              ),
+          filled: true,
+          fillColor:
+              readOnly == true
+                  ? grayColor.withValues(alpha: 0.4)
+                  : (backgroundColor ?? grayFormColor),
+          counterText: '',
+          hintStyle: styleSubtitle.copyWith(
+            color: hintTextColor ?? grayColor,
+            fontSize: screenWidth < screenHeight ? 16.sp : 10.sp,
+          ),
+          suffixIcon: showSuffixIcon ? suffixIcon : null,
+          prefixIcon: prefixIcon,
+          border: InputBorder.none,
+        ),
+        textCapitalization: capitalization ?? TextCapitalization.none,
+      ),
+    );
+  }
+}
